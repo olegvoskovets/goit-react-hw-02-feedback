6c5b05687c0de792101f7aa72defbf731945988d
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-05-25 23:18:06 +0300

    commit

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -4,7 +4,7 @@ import css from './Section.module.css';
 export const Section = ({ title, children }) => {
   return (
     <>
-      <h3>{title}</h3>
+      <h3 className={css.title}>{title}</h3>
       {children}
     </>
   );
